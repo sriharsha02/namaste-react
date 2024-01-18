dab6963974378d4b1c78ec94a5ab1623d19543b3
sriharsha02 <akshintalasriharsha@yahoo.com>
2024-01-18 21:53:04 +0530

    nested HTML in react

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React !"
-);
-console.log(heading);
-// console.log(heading.props.id);
+/*
+<div id="parent">
+    <div id = "child">
+        <h1> </h1>
+        <h2> </h2>
+    <div>
+
+</div>
+
+*/
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+]);
+console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(parent);
