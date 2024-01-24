6313f87ebcc3d6f0834c1f7a93b89c311f24378f
sriharsha02 <akshintalasriharsha@yahoo.com>
2024-01-24 22:15:27 +0530

    parcel features

--- App.js
+++ App.js
@@ -1,18 +1,10 @@
-/*
-<div id="parent">
-    <div id = "child">
-        <h1> </h1>
-        <h2> </h2>
-    <div>
-
-</div>
-
-*/
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
+    React.createElement("h1", {}, "This is Namaste React 🚀"),
+    React.createElement("h2", {}, "hello"),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I am h1 tag"),
--- README.md
+++ README.md
@@ -1,3 +1,24 @@
+#Namaste React
+
 This is my journey of learning react from scratch.
 
 The repo includes my notes, assignments and solutions, projects I practice.
+
+#Parcel
+
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - Written in C++
+- Caching - Faster builds
+- Image Optimization
+- Minification and bundling of files
+- Compressing
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling -Support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
