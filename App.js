6206c8d760ab9698fcea8cca268feebcff3472c8
sriharsha02 <akshintalasriharsha@yahoo.com>
2024-02-09 23:13:07 +0530

    props

--- App.js
+++ App.js
@@ -1,17 +1,754 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "This is Namaste React 🚀"),
-    React.createElement("h2", {}, "hello"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-]);
-console.log(parent);
+//React Element
+const Title = () => {
+  return <h1>Namaste React using JSX</h1>;
+};
+
+/**
+ * Header
+ *  -Logo
+ *  -NavItem
+ * Body
+ *  -Search
+ *  -RestaurantContainer
+ *    -RestaurantCard
+ *      -Image
+ *      -Name of the restaurant, star rating, cuisines, ETD
+ * Footer
+ *  -Copyright
+ *  -Links
+ *  -Address
+ *  -Contact  */
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = (props) => {
+  const { resData } = props;
+  return (
+    <div className="restaurant-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-logo"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          resData.cloudinaryImageId
+        }
+        alt="resLogo"
+      />
+      <h3>{resData.name}</h3>
+      <h5>{resData.cuisines}</h5>
+      <h5>{resData.avgRating} Star rating</h5>
+      <h5>{resData.costForTwo}</h5>
+      <h5>ETD:{resData.sla.deliveryTime} minutes</h5>
+    </div>
+  );
+};
+
+const resList = [
+  {
+    id: "64955",
+    name: "Srinidhi Veg Food Court",
+    cloudinaryImageId: "pljxtoxjtpn8wecz2icg",
+    locality: "Kalyan Nagar",
+    areaName: "Hrbr Layout",
+    costForTwo: "₹300 for two",
+    cuisines: ["South Indian", "Snacks", "North Indian", "Chinese"],
+    avgRating: 4.5,
+    parentId: "20161",
+    avgRatingString: "4.5",
+    totalRatingsString: "10K+",
+    sla: {
+      deliveryTime: 22,
+      lastMileTravel: 2.2,
+      serviceability: "SERVICEABLE",
+      slaString: "20-25 mins",
+      lastMileTravelString: "2.2 km",
+      iconType: "ICON_TYPE_EMPTY",
+    },
+    availability: {
+      nextCloseTime: "2024-02-09 22:25:00",
+      opened: true,
+    },
+    badges: {
+      imageBadges: [
+        {
+          imageId: "v1695133679/badges/Pure_Veg111.png",
+          description: "pureveg",
+        },
+      ],
+    },
+    isOpen: true,
+    aggregatedDiscountInfoV2: {},
+    type: "F",
+    badgesV2: {
+      entityBadges: {
+        imageBased: {
+          badgeObject: [
+            {
+              attributes: {
+                description: "pureveg",
+                imageId: "v1695133679/badges/Pure_Veg111.png",
+              },
+            },
+          ],
+        },
+        textBased: {},
+        textExtendedBadges: {},
+      },
+    },
+    orderabilityCommunication: {
+      title: {},
+      subTitle: {},
+      message: {},
+      customIcon: {},
+    },
+    differentiatedUi: {
+      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      differentiatedUiMediaDetails: {
+        mediaType: "ADS_MEDIA_ENUM_IMAGE",
+        lottie: {},
+        video: {},
+      },
+    },
+    reviewsSummary: {},
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    restaurantOfferPresentationInfo: {},
+  },
+  {
+    id: "30903",
+    name: "Sannidhi Grand",
+    cloudinaryImageId: "glun7fhhutgl0lkgekso",
+    locality: "Yadhav layout",
+    areaName: "Kammanahalli",
+    costForTwo: "₹150 for two",
+    cuisines: ["South Indian", "North Indian", "Chinese", "Beverages"],
+    avgRating: 4.2,
+    veg: true,
+    parentId: "21007",
+    avgRatingString: "4.2",
+    totalRatingsString: "10K+",
+    sla: {
+      deliveryTime: 29,
+      lastMileTravel: 3.5,
+      serviceability: "SERVICEABLE",
+      slaString: "25-30 mins",
+      lastMileTravelString: "3.5 km",
+      iconType: "ICON_TYPE_EMPTY",
+    },
+    availability: {
+      nextCloseTime: "2024-02-09 22:20:00",
+      opened: true,
+    },
+    badges: {
+      imageBadges: [
+        {
+          imageId: "v1695133679/badges/Pure_Veg111.png",
+          description: "pureveg",
+        },
+      ],
+    },
+    isOpen: true,
+    type: "F",
+    badgesV2: {
+      entityBadges: {
+        imageBased: {
+          badgeObject: [
+            {
+              attributes: {
+                description: "pureveg",
+                imageId: "v1695133679/badges/Pure_Veg111.png",
+              },
+            },
+          ],
+        },
+        textBased: {},
+        textExtendedBadges: {},
+      },
+    },
+    aggregatedDiscountInfoV3: {
+      header: "60% OFF",
+      subHeader: "UPTO ₹120",
+    },
+    orderabilityCommunication: {
+      title: {},
+      subTitle: {},
+      message: {},
+      customIcon: {},
+    },
+    differentiatedUi: {
+      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      differentiatedUiMediaDetails: {
+        mediaType: "ADS_MEDIA_ENUM_IMAGE",
+        lottie: {},
+        video: {},
+      },
+    },
+    reviewsSummary: {},
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    restaurantOfferPresentationInfo: {},
+  },
+  {
+    id: "327748",
+    name: "The Hole in the wall cafe",
+    cloudinaryImageId: "aduuzyfpu72fiz1wiheq",
+    locality: "3rd Block - Hrbr Layout",
+    areaName: "Kammanahalli/Kalyan Nagar",
+    costForTwo: "₹300 for two",
+    cuisines: ["American"],
+    avgRating: 4.5,
+    parentId: "11538",
+    avgRatingString: "4.5",
+    totalRatingsString: "5K+",
+    sla: {
+      deliveryTime: 32,
+      lastMileTravel: 3.7,
+      serviceability: "SERVICEABLE",
+      slaString: "30-35 mins",
+      lastMileTravelString: "3.7 km",
+      iconType: "ICON_TYPE_EMPTY",
+    },
+    availability: {
+      nextCloseTime: "2024-02-09 20:45:00",
+      opened: true,
+    },
+    badges: {
+      textExtendedBadges: [
+        {
+          iconId: "guiltfree/GF_Logo_android_3x",
+          shortDescription: "options available",
+          fontColor: "#7E808C",
+        },
+      ],
+    },
+    isOpen: true,
+    aggregatedDiscountInfoV2: {},
+    type: "F",
+    badgesV2: {
+      entityBadges: {
+        imageBased: {},
+        textBased: {},
+        textExtendedBadges: {
+          badgeObject: [
+            {
+              attributes: {
+                description: "",
+                fontColor: "#7E808C",
+                iconId: "guiltfree/GF_Logo_android_3x",
+                shortDescription: "options available",
+              },
+            },
+          ],
+        },
+      },
+    },
+    orderabilityCommunication: {
+      title: {},
+      subTitle: {},
+      message: {},
+      customIcon: {},
+    },
+    differentiatedUi: {
+      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      differentiatedUiMediaDetails: {
+        mediaType: "ADS_MEDIA_ENUM_IMAGE",
+        lottie: {},
+        video: {},
+      },
+    },
+    reviewsSummary: {},
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    restaurantOfferPresentationInfo: {},
+  },
+  {
+    id: "417538",
+    name: "Udupi Sannidhi Grand",
+    cloudinaryImageId: "zgkr3ss45lxr5fnybyyq",
+    locality: "Hennur Cross",
+    areaName: "Kalayan Nagar",
+    costForTwo: "₹200 for two",
+    cuisines: ["South Indian"],
+    avgRating: 4,
+    veg: true,
+    parentId: "232295",
+    avgRatingString: "4.0",
+    totalRatingsString: "1K+",
+    sla: {
+      deliveryTime: 20,
+      lastMileTravel: 1.3,
+      serviceability: "SERVICEABLE",
+      slaString: "15-20 mins",
+      lastMileTravelString: "1.3 km",
+      iconType: "ICON_TYPE_EMPTY",
+    },
+    availability: {
+      nextCloseTime: "2024-02-09 20:50:00",
+      opened: true,
+    },
+    badges: {
+      imageBadges: [
+        {
+          imageId: "v1695133679/badges/Pure_Veg111.png",
+          description: "pureveg",
+        },
+      ],
+    },
+    isOpen: true,
+    type: "F",
+    badgesV2: {
+      entityBadges: {
+        imageBased: {
+          badgeObject: [
+            {
+              attributes: {
+                description: "pureveg",
+                imageId: "v1695133679/badges/Pure_Veg111.png",
+              },
+            },
+          ],
+        },
+        textBased: {},
+        textExtendedBadges: {},
+      },
+    },
+    aggregatedDiscountInfoV3: {
+      header: "50% OFF",
+      subHeader: "UPTO ₹100",
+    },
+    orderabilityCommunication: {
+      title: {},
+      subTitle: {},
+      message: {},
+      customIcon: {},
+    },
+    differentiatedUi: {
+      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      differentiatedUiMediaDetails: {
+        mediaType: "ADS_MEDIA_ENUM_IMAGE",
+        lottie: {},
+        video: {},
+      },
+    },
+    reviewsSummary: {},
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    restaurantOfferPresentationInfo: {},
+  },
+  {
+    id: "781694",
+    name: "The Arogya Cafe",
+    cloudinaryImageId: "64e6a44484187682e2f28f7124803f4a",
+    locality: "Chanukya Layout",
+    areaName: "NAGAWARA",
+    costForTwo: "₹250 for two",
+    cuisines: ["Biryani", "Chinese"],
+    avgRating: 4.3,
+    veg: true,
+    parentId: "465313",
+    avgRatingString: "4.3",
+    totalRatingsString: "100+",
+    sla: {
+      deliveryTime: 36,
+      lastMileTravel: 3.8,
+      serviceability: "SERVICEABLE",
+      slaString: "35-40 mins",
+      lastMileTravelString: "3.8 km",
+      iconType: "ICON_TYPE_EMPTY",
+    },
+    availability: {
+      nextCloseTime: "2024-02-10 00:00:00",
+      opened: true,
+    },
+    badges: {},
+    isOpen: true,
+    aggregatedDiscountInfoV2: {},
+    type: "F",
+    badgesV2: {
+      entityBadges: {
+        imageBased: {},
+        textBased: {},
+        textExtendedBadges: {},
+      },
+    },
+    orderabilityCommunication: {
+      title: {},
+      subTitle: {},
+      message: {},
+      customIcon: {},
+    },
+    differentiatedUi: {
+      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      differentiatedUiMediaDetails: {
+        mediaType: "ADS_MEDIA_ENUM_IMAGE",
+        lottie: {},
+        video: {},
+      },
+    },
+    reviewsSummary: {},
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    isNewlyOnboarded: true,
+    restaurantOfferPresentationInfo: {},
+  },
+  {
+    id: "900",
+    name: "29 Degrees North (Pure Veg)",
+    cloudinaryImageId: "76354bd965db8dc9549b7b70cb444801",
+    locality: "Banaswadi",
+    areaName: "Banasawadi",
+    costForTwo: "₹250 for two",
+    cuisines: ["North Indian"],
+    avgRating: 4.5,
+    parentId: "7351",
+    avgRatingString: "4.5",
+    totalRatingsString: "10K+",
+    sla: {
+      deliveryTime: 33,
+      lastMileTravel: 4,
+      serviceability: "SERVICEABLE",
+      slaString: "30-35 mins",
+      lastMileTravelString: "4.0 km",
+      iconType: "ICON_TYPE_EMPTY",
+    },
+    availability: {
+      nextCloseTime: "2024-02-09 16:00:00",
+      opened: true,
+    },
+    badges: {
+      imageBadges: [
+        {
+          imageId: "v1695133679/badges/Pure_Veg111.png",
+          description: "pureveg",
+        },
+      ],
+      textExtendedBadges: [
+        {
+          iconId: "guiltfree/GF_Logo_android_3x",
+          shortDescription: "options available",
+          fontColor: "#7E808C",
+        },
+      ],
+    },
+    isOpen: true,
+    type: "F",
+    badgesV2: {
+      entityBadges: {
+        imageBased: {
+          badgeObject: [
+            {
+              attributes: {
+                description: "pureveg",
+                imageId: "v1695133679/badges/Pure_Veg111.png",
+              },
+            },
+          ],
+        },
+        textBased: {},
+        textExtendedBadges: {
+          badgeObject: [
+            {
+              attributes: {
+                description: "",
+                fontColor: "#7E808C",
+                iconId: "guiltfree/GF_Logo_android_3x",
+                shortDescription: "options available",
+              },
+            },
+          ],
+        },
+      },
+    },
+    aggregatedDiscountInfoV3: {
+      header: "10% OFF",
+      discountTag: "FLAT DEAL",
+    },
+    orderabilityCommunication: {
+      title: {},
+      subTitle: {},
+      message: {},
+      customIcon: {},
+    },
+    differentiatedUi: {
+      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      differentiatedUiMediaDetails: {
+        mediaType: "ADS_MEDIA_ENUM_IMAGE",
+        lottie: {},
+        video: {},
+      },
+    },
+    reviewsSummary: {},
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    restaurantOfferPresentationInfo: {},
+  },
+  {
+    id: "176267",
+    name: "Srinidhi Upachar",
+    cloudinaryImageId: "bhb7vveh3vlufijbgkfb",
+    locality: "Sarvagna Nagar",
+    areaName: "Banaswadi",
+    costForTwo: "₹300 for two",
+    cuisines: ["South Indian"],
+    avgRating: 4.4,
+    veg: true,
+    parentId: "20160",
+    avgRatingString: "4.4",
+    totalRatingsString: "5K+",
+    sla: {
+      deliveryTime: 25,
+      lastMileTravel: 3,
+      serviceability: "SERVICEABLE",
+      slaString: "25-30 mins",
+      lastMileTravelString: "3.0 km",
+      iconType: "ICON_TYPE_EMPTY",
+    },
+    availability: {
+      nextCloseTime: "2024-02-09 22:00:00",
+      opened: true,
+    },
+    badges: {
+      imageBadges: [
+        {
+          imageId: "v1695133679/badges/Pure_Veg111.png",
+          description: "pureveg",
+        },
+      ],
+    },
+    isOpen: true,
+    aggregatedDiscountInfoV2: {},
+    type: "F",
+    badgesV2: {
+      entityBadges: {
+        imageBased: {
+          badgeObject: [
+            {
+              attributes: {
+                description: "pureveg",
+                imageId: "v1695133679/badges/Pure_Veg111.png",
+              },
+            },
+          ],
+        },
+        textBased: {},
+        textExtendedBadges: {},
+      },
+    },
+    orderabilityCommunication: {
+      title: {},
+      subTitle: {},
+      message: {},
+      customIcon: {},
+    },
+    differentiatedUi: {
+      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      differentiatedUiMediaDetails: {
+        mediaType: "ADS_MEDIA_ENUM_IMAGE",
+        lottie: {},
+        video: {},
+      },
+    },
+    reviewsSummary: {},
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    restaurantOfferPresentationInfo: {},
+  },
+  {
+    id: "52934",
+    name: "Udupi Gardenia",
+    cloudinaryImageId: "hm9alpsl6gvphjgfyyxc",
+    locality: "Munireddy layout",
+    areaName: "Banasawadi",
+    costForTwo: "₹200 for two",
+    cuisines: [
+      "South Indian",
+      "North Indian",
+      "Desserts",
+      "Chinese",
+      "Chaat",
+      "Ice Cream",
+      "Juices",
+    ],
+    avgRating: 4.5,
+    veg: true,
+    parentId: "219016",
+    avgRatingString: "4.5",
+    totalRatingsString: "10K+",
+    sla: {
+      deliveryTime: 24,
+      lastMileTravel: 3,
+      serviceability: "SERVICEABLE",
+      slaString: "20-25 mins",
+      lastMileTravelString: "3.0 km",
+      iconType: "ICON_TYPE_EMPTY",
+    },
+    availability: {
+      nextCloseTime: "2024-02-09 22:30:00",
+      opened: true,
+    },
+    badges: {
+      imageBadges: [
+        {
+          imageId: "v1695133679/badges/Pure_Veg111.png",
+          description: "pureveg",
+        },
+      ],
+    },
+    isOpen: true,
+    aggregatedDiscountInfoV2: {},
+    type: "F",
+    badgesV2: {
+      entityBadges: {
+        imageBased: {
+          badgeObject: [
+            {
+              attributes: {
+                description: "pureveg",
+                imageId: "v1695133679/badges/Pure_Veg111.png",
+              },
+            },
+          ],
+        },
+        textBased: {},
+        textExtendedBadges: {},
+      },
+    },
+    orderabilityCommunication: {
+      title: {},
+      subTitle: {},
+      message: {},
+      customIcon: {},
+    },
+    differentiatedUi: {
+      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      differentiatedUiMediaDetails: {
+        mediaType: "ADS_MEDIA_ENUM_IMAGE",
+        lottie: {},
+        video: {},
+      },
+    },
+    reviewsSummary: {},
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    restaurantOfferPresentationInfo: {},
+  },
+  {
+    id: "240552",
+    name: "Udupi Gardenia (jayanthinagar)",
+    cloudinaryImageId: "lfrcp0wsmohpowae3k26",
+    locality: "Jayanthi Nagar",
+    areaName: "Ramamurthy nagar",
+    costForTwo: "₹250 for two",
+    cuisines: [
+      "South Indian",
+      "North Indian",
+      "Chinese",
+      "Juices",
+      "Ice Cream",
+      "Chaat",
+    ],
+    avgRating: 4.4,
+    veg: true,
+    parentId: "20144",
+    avgRatingString: "4.4",
+    totalRatingsString: "10K+",
+    sla: {
+      deliveryTime: 25,
+      lastMileTravel: 3,
+      serviceability: "SERVICEABLE",
+      slaString: "25-30 mins",
+      lastMileTravelString: "3.0 km",
+      iconType: "ICON_TYPE_EMPTY",
+    },
+    availability: {
+      nextCloseTime: "2024-02-09 22:30:00",
+      opened: true,
+    },
+    badges: {
+      imageBadges: [
+        {
+          imageId: "v1695133679/badges/Pure_Veg111.png",
+          description: "pureveg",
+        },
+      ],
+    },
+    isOpen: true,
+    type: "F",
+    badgesV2: {
+      entityBadges: {
+        imageBased: {
+          badgeObject: [
+            {
+              attributes: {
+                description: "pureveg",
+                imageId: "v1695133679/badges/Pure_Veg111.png",
+              },
+            },
+          ],
+        },
+        textBased: {},
+        textExtendedBadges: {},
+      },
+    },
+    aggregatedDiscountInfoV3: {
+      header: "15% OFF",
+      subHeader: "ABOVE ₹700",
+      discountTag: "FLAT DEAL",
+    },
+    orderabilityCommunication: {
+      title: {},
+      subTitle: {},
+      message: {},
+      customIcon: {},
+    },
+    differentiatedUi: {
+      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      differentiatedUiMediaDetails: {
+        mediaType: "ADS_MEDIA_ENUM_IMAGE",
+        lottie: {},
+        video: {},
+      },
+    },
+    reviewsSummary: {},
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    restaurantOfferPresentationInfo: {},
+  },
+];
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="restaurant-container">
+        {resList.map((restaurant) => (
+          <RestaurantCard /**key={restaurant.id}*/ resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<AppLayout />);
